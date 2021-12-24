90901dd68388327996a3596196f5a592185373b9
ChironUpgrades <94480440+ChironUpgrades@users.noreply.github.com>
2021-12-24 01:07:21 +0100

    Update Configuration.h

--- Configuration.h
+++ Configuration.h
@@ -1179,7 +1179,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 10, 10, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -38, 3, -2.66 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
